8aafe6541775a9867f55328d33b6d31a64c802ab
Miloslav Milenkov <mmilenkov@hotmail.co.uk>
2019-05-12 18:21:03 +0300

    Version 1 finished

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -14,8 +14,10 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
 
